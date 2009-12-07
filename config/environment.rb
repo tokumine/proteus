99587d02f1bd48573ca8389a1f59cb18e71d8e5b
Simon Tokumine <simon@japancentre.com>
2009-12-07 13:10:59 +0000

    installed and setup gems

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+   config.gem "uuid"
+   config.gem 'state_machine'
+   config.gem "paperclip"
+   config.gem "GeoRuby", :lib => "geo_ruby"
+   config.gem "postgis_adapter"
+   config.gem 'will_paginate', :version => '2.3.11'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -39,3 +44,7 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+#RESET DEFAULT SRID THAT GEORUBY USES (GEM DEFAULTS TO -1)
+GeoRuby::SimpleFeatures::DEFAULT_SRID = 4326
+
