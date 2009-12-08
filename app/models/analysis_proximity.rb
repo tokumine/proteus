7f01b2109addf37131f5e20c74e8d78961fccf6c
Simon Tokumine <simon@japancentre.com>
2009-12-08 13:07:11 +0000

    updated styles

--- app/models/analysis_proximity.rb
+++ app/models/analysis_proximity.rb
@@ -1,9 +1,9 @@
 class AnalysisProximity < Analysis
   
-  #selects PAs, outputs as geojson with distance that are within 50 degrees of the input polygon
+  #selects PAs, outputs as geojson with distance that are within 0.5 degrees of the input polygon
   def self.analyse tenement_id, wkt
     return nil unless wkt
-    results = self.find_by_sql "select gid as result_id, ST_Distance(the_geom, ST_GeomFromEWKT('SRID=4326;#{wkt}')) as distance from pas where the_geom && ST_Buffer(ST_GeomFromEWKT('SRID=4326;#{wkt}'),50)"
+    results = self.find_by_sql "select gid as result_id, ST_Distance(the_geom, ST_GeomFromEWKT('SRID=4326;#{wkt}')) as distance from pas where the_geom && ST_Buffer(ST_GeomFromEWKT('SRID=4326;#{wkt}'),0.5)"
     
    analysis = []
     results.each do |r|
