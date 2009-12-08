358369ff9aef51b307a97345243b807e516a9bee
Simon Tokumine <simon@japancentre.com>
2009-12-08 12:53:19 +0000

    updated styles

--- app/models/tenement.rb
+++ app/models/tenement.rb
@@ -2,6 +2,15 @@ class Tenement < ActiveRecord::Base
   belongs_to :assesment
   has_many :analyses, :dependent => :destroy
   has_many :pas, :through => :analyses
+  has_many :overlapping_pas, :through => :analyses, 
+                             :source => :pa,
+                             :conditions => "analyses.type = 'AnalysisOverlap'",
+                             :order =>'pas.name_eng ASC'
+  has_many :nearby_pas,     :through => :analyses, 
+                            :source => :pa,
+                            :conditions => "analyses.type = 'AnalysisProximity'",
+                            :order =>'pas.name_eng ASC'
+  
   acts_as_geom :the_geom => :polygon
   
 end
