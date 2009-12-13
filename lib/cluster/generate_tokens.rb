f7f49709c56135005af7080e94e09affaf959d9e
Simon Tokumine <simon@japancentre.com>
2009-12-13 22:13:32 +0000

    Added dictionary create for initial pa clustering step

--- lib/cluster/generate_tokens.rb
+++ lib/cluster/generate_tokens.rb
@@ -1,3 +1,7 @@
+#CLUSTERING  OF PA'S FOR ABSTRACT AND RELATEDNESS MEASURES
+
+#1) AIM: CREATE A DICTIONARY OF "BAD TERMS" TO EXCLUDE FROM FURTHER ANALYSIS
+#   METHOD: TOKENISE ALL WORDS AND HISTOGRAM. SORT.
 tokens = {}
 
 pas = Pa.find_each( :select => 'DISTINCT name_eng, gid', :batch_size => 100 ) do |p|
@@ -8,4 +12,9 @@ pas = Pa.find_each( :select => 'DISTINCT name_eng, gid', :batch_size => 100 ) do
 end
 sorted_tokens = tokens.sort {|a,b| b[1]<=>a[1]}
 
-puts sorted_tokens.inspect
+puts sorted_tokens.to_yaml
+
+
+#2) DECIDE HOW TO SPLIT HISTOGRAM TO REMOVE COMMON WORDS - THRESHOLD? USE SECOND DERIVATIVE?
+
+#3) 
